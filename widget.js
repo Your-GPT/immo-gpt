fd4dce2b6aeb5f515db82cf121dc7825b4634636
Your-GPT <kontakt@your-gpt.de>
2024-10-10 14:16:55 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -232,8 +232,8 @@ df-messenger::part(chat-bubble):hover {
 
         @media (max-width: 480px) {
       df-messenger {
-      --df-messenger-chat-window-height: 450px;
-      --df-messenger-chat-window-width: 300px;
+      --df-messenger-chat-window-height: 100vh;
+      --df-messenger-chat-window-width: 100vw;
     }
       }
   `;

279b41be419b02211954fcc275fb07d2e4cd4429
Your-GPT <kontakt@your-gpt.de>
2024-10-18 11:50:47 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -10,54 +10,53 @@
     }
 
     .cb-widget-buttons {
-      position: fixed;
-      bottom: 20px;
-      right: 78px;
-      display: flex;
-      flex-direction: row-reverse;
-      gap: 10px;
-      z-index: 998;
-      transition: transform 0.5s ease, opacity 0.5s ease;
-    }
+            position: fixed;
+            bottom: 20px;
+            right: 78px;
+            display: flex;
+            flex-direction: row-reverse;
+            gap: 10px;
+            z-index: 998;
+            transition: transform 0.5s ease, opacity 0.5s ease;
+        }
 
-    .cb-chat-wrapper {
-    border-radius: 50%;
-    }
+        .cb-widget-button {
+            background-color: var(--widget-button-color);
+            color: var(--widget-icon-color);
+            border: none;
+            border-radius: 50%;
+            width: 48px;
+            height: 48px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 24px;
+            cursor: pointer;
+            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+            transition: background-color 0.3s ease, box-shadow 0.3s ease, transform 0.5s ease, opacity 0.5s ease;
+            position: relative;
+        }
 
-    .cb-widget-button {
-      background-color: var(--widget-button-color);
-      color: var(--widget-icon-color);
-      border: none;
-      border-radius: 50%;
-      width: 48px;
-      height: 48px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-size: 24px;
-      cursor: pointer;
-      box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
-      transition: background-color 0.3s ease, box-shadow 0.3s ease, transform 0.5s ease, opacity 0.5s ease;
-      position: relative;
-    }
+        .cb-widget-button svg {
+            pointer-events: none;
+            width: 24px;
+            height: 24px;
+            fill: currentColor;
+        }
 
-    .cb-widget-button i {
-      pointer-events: none;
-    }
+        .cb-widget-button:hover {
+            background-color: var(--widget-button-hover-color);
+            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.15);
+        }
 
-    .cb-widget-button:hover {
-      background-color: var(--widget-button-hover-color);
-      box-shadow: 0 4px 20px rgba(0, 0, 0, 0.15);
-    }
+        .cb-widget-buttons.collapsed {
+            transform: translateX(70px);
+            opacity: 0;
+        }
 
-    .cb-widget-buttons.collapsed {
-      transform: translateX(70px);
-      opacity: 0;
-    }
-
-    .cb-widget-buttons.collapsed .widget-button {
-      transform: scale(0);
-    }
+        .cb-widget-buttons.collapsed .widget-button {
+            transform: scale(0);
+        }
 
     .cb-button-stack-area {
       position: fixed;
@@ -200,22 +199,28 @@ df-messenger:hover::part(chat-bubble) {
       transform: translateY(10px);
     }
 
-    .cb-social-icons {
-      display: flex;
-      justify-content: space-around;
-      margin-top: 5px;
-    }
+    .social-icons {
+            display: flex;
+            justify-content: space-around;
+            margin-top: 5px;
+        }
 
-    .cb-social-icons a {
-      color: var(--widget-button-color);
-      font-size: 18px;
-      transition: color 0.3s ease, filter 0.3s ease;
-    }
+        .social-icons a {
+            color: var(--widget-button-color);
+            font-size: 18px;
+            transition: color 0.3s ease, filter 0.3s ease;
+        }
 
-    .cb-social-icons a:hover {
-      color: var(--widget-button-hover-color);
-      filter: brightness(1.5);
-    }
+        .social-icons a:hover {
+            color: var(--widget-button-hover-color);
+            filter: brightness(1.5);
+        }
+
+        .social-icons svg {
+            width: 18px;
+            height: 18px;
+            fill: currentColor;
+        }
 
     .grecaptcha-badge {
       visibility: hidden;
@@ -258,15 +263,21 @@ df-messenger:hover::part(chat-bubble) {
   const widgetButtons = document.createElement('div');
   widgetButtons.className = 'cb-widget-buttons';
   widgetButtons.innerHTML = `
-    <button class="cb-widget-button" id="cbsearchWidgetTrigger">
-      <div class="fas fa-search"></div>
-    </button>
-    <button class="cb-widget-button" onclick="window.location.href='tel:+491234567890';">
-      <div class="fas fa-phone"></div>
-    </button>
-    <button class="cb-widget-button" onclick="window.location.href='mailto:info@goost-immobilien.de';">
-      <div class="fas fa-envelope"></div>
-    </button>
+    <button class="cb-widget-button" id="searchWidgetTrigger">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/>
+            </svg>
+        </button>
+        <button class="cb-widget-button" onclick="window.location.href='tel:+491234567890';">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"/>
+            </svg>
+        </button>
+        <button class="cb-widget-button" onclick="window.location.href='mailto:info@goost-immobilien.de';">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"/>
+            </svg>
+        </button>
   `;
   document.body.appendChild(widgetButtons);
 
@@ -308,15 +319,31 @@ df-messenger:hover::part(chat-bubble) {
     popup.innerHTML = message;
 
     if (socialIcons) {
-      popup.innerHTML += `
-        <div class="cb-social-icons">
-          <a href="https://www.instagram.com/goost_immobilien/" target="_blank"><div class="fab fa-instagram"></div></a>
-          <a href="https://www.facebook.com/Goost.Immobilien/" target="_blank"><div class="fab fa-facebook"></div></a>
-          <a href="https://www.youtube.com/channel/UCnLEXs3sZmUv6zQYIPko6UQ" target="_blank"><div class="fab fa-youtube"></div></a>
-          <a href="https://www.linkedin.com/company/goost-immobilien/" target="_blank"><div class="fab fa-linkedin"></div></a>
-        </div>
-      `;
-    }
+                    popup.innerHTML += `
+                        <div class="social-icons">
+                            <a href="https://www.instagram.com/goost_immobilien/" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+                                </svg>
+                            </a>
+                            <a href="https://www.facebook.com/Goost.Immobilien/" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                    <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/>
+                                </svg>
+                            </a>
+                            <a href="https://www.youtube.com/channel/UCnLEXs3sZmUv6zQYIPko6UQ" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                    <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/>
+                                </svg>
+                            </a>
+                            <a href="https://www.linkedin.com/company/goost-immobilien/" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
+                                </svg>
+                            </a>
+                        </div>
+                    `;
+                }
 
     chatPopupContainer.insertBefore(popup, chatPopupContainer.firstChild);
 
@@ -373,7 +400,7 @@ df-messenger:hover::part(chat-bubble) {
     chatPopupContainer.addEventListener('click', function(event) {
       const popup = event.target.closest('.cb-chat-popup');
       if (popup) {
-        if (event.target.closest('.cb-social-icons a')) {
+        if (event.target.closest('.social-icons a')) {
           return;
         }
         const dfMessenger = document.querySelector('df-messenger');
